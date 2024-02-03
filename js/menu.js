9898a0787a21b319f3b70069c0f6ef38fc469770
MrGamp2411 <155970537+MrGamp2411@users.noreply.github.com>
2024-02-03 05:56:01 +0100

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,18 @@
-document.querySelector(".menu-icon").addEventListener("click", function() {
+let isMenuOpen = false;
+
+function toggleMenu() {
     console.log("Funzione toggleMenu chiamata");
     var nav = document.getElementById("myNav");
+
+    isMenuOpen = !isMenuOpen;
+
+    if (isMenuOpen) {
+        nav.style.maxHeight = "400px"; /* o qualsiasi altra altezza desiderata */
+    } else {
+        nav.style.maxHeight = "0";
+    }
+
     nav.classList.toggle("open");
-});
+}
+
+document.querySelector(".menu-icon").addEventListener("click", toggleMenu);
