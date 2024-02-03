ff433e12a1e53fa42b4db749919afd0fc5dbf66b
MrGamp2411 <155970537+MrGamp2411@users.noreply.github.com>
2024-02-03 04:37:27 +0100

    Delete js directory

--- js/custom.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".menu-icon").addEventListener("click", function() {
-    console.log("Funzione toggleMenu chiamata");
-    var nav = document.getElementById("myNav");
-    nav.classList.toggle("open");
-});
